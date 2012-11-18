8e11a9b6b9ec8ead84e7f2b49f3777567d7858d0
hans.php <hans.php@15e16404-1e75-bd53-30ad-7f0184d9f9a1>
2012-11-18 19:38:37 +0000

    Optimización de argumentos y comentarios. ----> se agrega evento Wheel para generar el movimiento del ratón. ..

--- Programa/src/laberintoraton/PanelTablero.java
+++ Programa/src/laberintoraton/PanelTablero.java
@@ -5,11 +5,12 @@ import java.awt.Graphics;
 import java.awt.Image;
 import java.awt.Toolkit;
 import java.awt.event.MouseEvent;
+import java.awt.event.MouseWheelEvent;
 
 import javax.swing.JPanel;
 
 /**
- * Panel para el dibujo de un Tablero de juego asociado a un objeto de la clase
+ * Panel para el dibujo de un Tablero de asociado a un objeto de la clase
  * Tablero
  */
 class PanelTablero extends JPanel {
@@ -17,6 +18,8 @@ class PanelTablero extends JPanel {
 	private static final long serialVersionUID = -2957846057828458109L;
 
 	Tablero t;
+	
+	int dire;
 
 	public PanelTablero(Tablero t) {
 		this.t = t;
@@ -40,7 +43,7 @@ class PanelTablero extends JPanel {
 				double dr = (y - 10) / (t.largo * 1.0);
 				int c = (int) Math.floor(dc);
 				int r = (int) Math.floor(dr);
-				if ((r >= 0 && r < t.ren) && (c >= 0 && c < t.col)) {
+				if ((r >= 0 && r < t.fila) && (c >= 0 && c < t.col)) {
 					System.out.printf("(%d,%d) -->  [%2d,%2d]\n", x, y, r, c);
 					t.jugada(r, c);
 				}
@@ -60,31 +63,44 @@ class PanelTablero extends JPanel {
 
 			@Override
 			public void mouseReleased(MouseEvent e) {
+				
 			}
 		});
 
-	}
+		this.addMouseWheelListener(new  java.awt.event.MouseWheelListener(){
 
-	static char[] s = { 'l', 'a', 'd', 'r', 'i', 'l', 'l', 'o' };
+			@Override
+			public void mouseWheelMoved(MouseWheelEvent e) {
+				// TODO Auto-generated method stub
+				System.out.println("WHELL:"+e.getWheelRotation());
+			}
+				
+		});
+			
+		
+	}
 
 	@Override
 	public void paint(Graphics g) {
-
 		int y = 10;
-		for (int r = 0; r < t.ren; r++) {
+		for (int f = 0; f < t.fila; f++) {
 			int x = 10;
 			for (int c = 0; c < t.col; c++) {
-				g.setColor(Color.WHITE);
+				
+				g.setColor(Color.LIGHT_GRAY);
 				g.drawRect(x, y, t.largo, t.largo);
-				g.setColor(t.color(r, c));
-				g.fillRect(x + 1, y + 1, t.largo - 2, t.largo - 2);
+				
+				g.setColor(t.color(f, c));
+				g.fillRect(x + 1, y + 1, t.largo -2 , t.largo -2 );
+				
 				x += t.largo;
 			}
 			y += t.largo;
 		}
-		g.drawChars(s, 0, 8, 20, (t.ren + 1) * t.largo + 10);
-		g.setColor(Color.black);
-		g.fillRect(20, (t.ren + 1) * t.largo + 15, 80, 20);
+		g.setColor(Color.BLACK);
+		g.drawString("Ladrillos", 20, (t.fila + 1) * t.largo + 10);
+		
+		g.fillRect(20, (t.fila + 1) * t.largo + 15, 80, 20);
 		Image raton, salida;
 		raton = Toolkit.getDefaultToolkit().getImage("raton.png");
 		g.drawImage(raton, 10, 10, 32, 32, this);
--- Programa/src/laberintoraton/Principal.java
+++ Programa/src/laberintoraton/Principal.java
@@ -3,8 +3,8 @@ package laberintoraton;
 public class Principal {
 
 	public static void main(String s[]) {
-		Tablero tablero = new Tablero(15, 15, 32);
-		System.out.println("Creando tablero");
+		Tablero tablero = new Tablero(15, 10);
 		new FrmTablero(tablero);
 	}
+	
 }
