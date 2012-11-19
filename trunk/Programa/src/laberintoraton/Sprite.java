956508fc995ab9eebd96d23dc4d0ed50af50992e
hans.php <hans.php@15e16404-1e75-bd53-30ad-7f0184d9f9a1>
2012-11-19 22:04:42 +0000

    Cración de la clase Sprite.java para la impresión de imagenes en el Stage además de la programación de los eventos para la seleción de elementos.

---
+++ trunk/Programa/src/laberintoraton/Sprite.java
@@ -0,0 +1,46 @@
+package laberintoraton;
+
+import java.awt.Color;
+import java.awt.Graphics;
+import java.awt.Image;
+import java.awt.Toolkit;
+
+public class Sprite {
+	
+	static int gX;
+	static Tablero t;
+	static PanelTablero obj;
+	static String actual="";
+	
+	Image pic;
+	String nombre;	
+	
+	Sprite(String n, PanelTablero este){
+		obj = este;
+		t = obj.t;
+		gX =  (t.col*t.largo)+20;
+		validadorInicial(n);
+	}
+	Sprite(String n){
+		validadorInicial(n);
+	}
+	
+	private boolean validadorInicial(String n){
+		if("raton"==n || "salida"==n || "pared"==n || "goma"==n){
+			pic = Toolkit.getDefaultToolkit().getImage(n+".png");
+			nombre=n;
+			return true;
+		}else{
+			return false;
+		}
+	}
+	void dibujar(Graphics g, int x, int y){
+		g.drawImage(pic, x, y, t.largo, t.largo, obj);
+	}
+	void dibujar(Graphics g, int y){
+		g.drawImage(pic, gX, y, t.largo, t.largo, obj);
+		g.setColor(Color.BLACK);
+		g.drawString(nombre.toUpperCase(),  gX+40, y+20);
+	}
+
+}
